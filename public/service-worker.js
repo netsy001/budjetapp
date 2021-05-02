1104107da021756c8a71006b31aea8fcb15cffa6
Surendra Choudary Nettam <nettem.surendra@gmail.com>
2021-05-02 20:26:59 +0930

    manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,22 @@
 const FILES_TO_CACHE = [
     '/',
     '/index.html',
+    '/index.js',
+    '/style.css',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
-    '/dist/favorites.bundle.js',
-    '/dist/topic.bundle.js',
+    `/manifest.webmanifest`,
     'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
     'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
 ];
 
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+const STATIC_CACHE = 'static-cache-v1';
+const RUNTIME_CACHE = 'runtime-cache';
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches
-        .open(PRECACHE)
+        .open(STATIC_CACHE)
         .then((cache) => cache.addAll(FILES_TO_CACHE))
         .then(self.skipWaiting())
     );
@@ -23,7 +24,7 @@ self.addEventListener('install', (event) => {
 
 // The activate handler takes care of cleaning up old caches.
 self.addEventListener('activate', (event) => {
-    const currentCaches = [PRECACHE, RUNTIME];
+    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
     event.waitUntil(
         caches
         .keys()
@@ -41,22 +42,42 @@ self.addEventListener('activate', (event) => {
     );
 });
 
-self.addEventListener('fetch', (event) => {
-    if (event.request.url.startsWith(self.location.origin)) {
-        event.respondWith(
-            caches.match(event.request).then((cachedResponse) => {
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
-
-                return caches.open(RUNTIME).then((cache) => {
-                    return fetch(event.request).then((response) => {
-                        return cache.put(event.request, response.clone()).then(() => {
-                            return response;
-                        });
-                    });
-                });
-            })
-        );
+self.addEventListener(`fetch`, event => {
+    if (
+        event.request.method !== `GET` ||
+        !event.request.url.startsWith(self.location.origin)
+    ) {
+        event.respondWith(fetch(event.request));
+        return;
     }
+
+    if (event.request.url.includes(`/api/transaction`)) {
+        event.respondWith(
+            caches.open(RUNTIME_CACHE).then(cache =>
+                fetch(event.request)
+                .then(response => {
+                    cache.put(event.request, response.clone());
+                    return response;
+                })
+                .catch(() => caches.match(event.request))
+            )
+        );
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+
+            return caches
+                .open(RUNTIME_CACHE)
+                .then(cache =>
+                    fetch(event.request).then(response =>
+                        cache.put(event.request, response.clone()).then(() => response)
+                    )
+                );
+        })
+    );
 });
